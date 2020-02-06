90d08bf1a972686eeeee7df48c5e35bd2fe2e930
dnesbitt03qub <dnesbitt03@qub.ac.uk>
2020-02-06 12:54:28 +0000

    Fix bug resulting in non-standard EPUB output

--- bouncer/epub_handling.py
+++ bouncer/epub_handling.py
@@ -10,6 +10,7 @@ from zipfile import ZipFile
 import glob
 import os
 
+# Extract all the files from the EPUB archive into the dest_folder
 def extract_from_epub_file(path, dest_folder):
     # Open the zip file
     try:
@@ -20,20 +21,27 @@ def extract_from_epub_file(path, dest_folder):
         print(f'Error opening ePub file {path}, does it exist?')
         quit()
         
-# Get a list of the files in the folder, with mimetype the first in the list
+# Get a list of the files in the folder, with mimetype as the first in the list
 def list_of_files_in_folder(folder: str):
     folder_glob = folder + '/**'
     files = glob.glob(folder_glob, recursive=True)
     files = [ file for file in files if os.path.isfile(file)]
+
+    # Put mimetype file as the first in the list
     mimetype_index = files.index(folder + '/mimetype')
     files[0], files[mimetype_index] = files[mimetype_index], files[0]
+    
     return files
-        
+
+# Write all the files in the source_folder to an EPUB archive
 def write_epub_file(path, source_folder):
     # Zip the temp folder
     with ZipFile(path, 'w') as zip_file:
-        # Write the files in temp (with any corrections) to corrected filename
-        # NOTE: standard requires mimetype file be first
+        # Write the files in source_file (with any corrections) to corrected filename
+        # NOTE: standard requires mimetype file be first file in archive
         files_to_write = list_of_files_in_folder(source_folder)
+
         for file in files_to_write:
-            zip_file.write(file)
+            # Remove the temp folder name before writing
+            file_name_in_zip = file[len(source_folder)+1:]
+            zip_file.write(file, file_name_in_zip)
