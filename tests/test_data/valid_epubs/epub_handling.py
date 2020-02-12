b3c002115be2e355810d2aa2a7c1a8c70d48c14e
dnesbitt03qub <dnesbitt03@qub.ac.uk>
2020-02-12 14:38:26 +0000

    Add a public domain epub for testing & add more unit testing

---
+++ tests/test_data/valid_epubs/epub_handling.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  5 10:39:05 2020
+
+@author: plusuncold
+"""
+
+from zipfile import ZipFile, ZIP_DEFLATED, ZIP_STORED
+import glob
+import os
+
+
+# Extract all the files from the EPUB archive into the dest_folder
+def extract_from_epub_file(path, dest_folder):
+    # Open the zip file
+    try:
+        with ZipFile(path) as zip_file:
+            # Extract to temp folder
+            zip_file.extractall(dest_folder)
+    except Exception as ex:
+        print(f'Error opening ePub file {path}, does it exist?')
+        print(ex.args)
+        quit()
+
+
+# Get a list of the files in the folder, with mimetype as the first in the list
+def list_of_files_in_folder(folder: str):
+    folder_glob = folder + '/**'
+    files = glob.glob(folder_glob, recursive=True)
+    files = [file for file in files if os.path.isfile(file)]
+
+    # Put mimetype file as the first in the list
+    mimetype_index = files.index(folder + '/mimetype')
+    files[0], files[mimetype_index] = files[mimetype_index], files[0]
+
+    return files
+
+
+# Write all the files in the source_folder to an EPUB archive
+def write_epub_file(path, source_folder):
+    # Zip the temp folder
+    with ZipFile(path, 'w') as zip_file:
+        # Write the files in source_file (with any corrections)
+        # to corrected filename
+        # NOTE: standard requires mimetype file be first file in archive
+        files_to_write = list_of_files_in_folder(source_folder)
+
+        for index, file in enumerate(files_to_write):
+            # Remove the temp folder name before writing
+            file_name_in_zip = file[len(source_folder)+1:]
+            # Compress all members with compress_type 8, apart from mimetype
+            compress_type = ZIP_DEFLATED if index > 0 else ZIP_STORED
+            zip_file.write(file, file_name_in_zip, compress_type=compress_type)
