41196c31d2304eef417854e51ffba659b94d3afa
dnesbitt03qub <dnesbitt03@qub.ac.uk>
2020-02-12 14:36:38 +0000

    Split out main program loop for easier unit testing

--- bouncer/bouncer.py
+++ bouncer/bouncer.py
@@ -21,6 +21,50 @@ def delete_folder(folder: str):
         print(f'Error deleting temp folder: {ex.filename} {ex.strerror}')
 
 
+def correct_ebook(epub_name: str, temp_folder: str, dict_lang: str):
+    # set path and path_corrected
+    path = epub_name
+    if '.epub' not in epub_name[-5:]:
+        path = epub_name + '.epub'
+    path_corrected = path[:-5] + '_corrected.epub'
+
+    # Only permit currently allowable dictionaries
+    if dict_lang != 'en_US':
+        print(f'Bouncer does not support {dict_lang} currently only')
+        print(' the en_US dictionary is supported')
+        quit()
+
+    # Extract the files from the epub into temp_folder
+    epub_handling.extract_from_epub_file(path, temp_folder)
+
+    print('Extracted the ePub files')
+
+    # Get the location of all the files with text in
+    contents_path = xh.get_contents_path_from_container_file(temp_folder)
+    text_files = xh.get_text_file_paths_from_contents_file(contents_path,
+                                                           temp_folder)
+
+    # Read list of paragraphs from text files
+    text_from_files = xh.get_text_elements_from_text_files(text_files)
+
+    print('All text read from ePub')
+
+    # Get dict of corrections [original,correction] to be applied
+    # to all text_files
+    corrections = correct_spellings.corrections_for_words(text_from_files,
+                                                          dict_lang)
+
+    print(f'Applying corrections to ePub extracted files...')
+    xh.apply_corrections(corrections, text_files)
+
+    print(f'Applied corrections, writing extracted files back to .ePub file')
+    epub_handling.write_epub_file(path_corrected, temp_folder)
+
+    print('Wrote corrected ePub!')
+
+    delete_folder(temp_folder)
+
+
 def main():
     # Read arguments
     parser = argparse.ArgumentParser()
@@ -32,46 +76,7 @@ def main():
                         help='name of the temporary folder created')
     args = parser.parse_args()
 
-    # set path and path_corrected
-    path = args.epub_name
-    if '.epub' not in args.epub_name[-5:]:
-        path = args.epub_name + '.epub'
-    path_corrected = path[:-5] + '_corrected.epub'
-
-    # Only permit currently allowable dictionaries
-    if args.dict_lang != 'en_US':
-        print(f'Bouncer currently only supports the en_US dictionary')
-        quit()
-
-    # Extract the files from the epub into temp_folder
-    epub_handling.extract_from_epub_file(path, args.temp_folder)
-
-    print('Extracted the ePub files')
-
-    # Get the location of all the files with text in
-    contents_path = xh.get_contents_path_from_container_file(args.temp_folder)
-    text_files = xh.get_text_file_paths_from_contents_file(contents_path,
-                                                           args.temp_folder)
-
-    # Read list of paragraphs from text files
-    text_from_files = xh.get_text_elements_from_text_files(text_files)
-
-    print('All text read from ePub')
-
-    # Get dict of corrections [original,correction] to be applied
-    # to all text_files
-    corrections = correct_spellings.corrections_for_words(text_from_files,
-                                                          args.dict_lang)
-
-    print(f'Applying corrections to ePub extracted files...')
-    xh.apply_corrections(corrections, text_files)
-
-    print(f'Applied corrections, writing extracted files back to .ePub file')
-    epub_handling.write_epub_file(path_corrected, args.temp_folder)
-
-    print('Wrote corrected ePub!')
-
-    delete_folder(args.temp_folder)
+    correct_ebook(args.epub_name, args.temp_folder, args.dict_lang)
 
 
 if __name__ == '__main__':
